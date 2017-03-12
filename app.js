0b1a70ec75bd13ff4b19b7285116c891f9c258cb
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-03-12 01:08:52 -0800

    handle edits properly

--- app.js
+++ app.js
@@ -51,54 +51,75 @@ class App extends MatrixPuppetBridgeBase {
   }
   registerMessageListener() {
     this.client.on('message', (data)=>{
-      const { channel, user, text, attachments, subtype, bot_id } = data;
-
-      if (subtype === "message_changed") {
-        debug('ignoring an edit');
-        return;
-      }
-
-      // any direct text
-      let messages = [text];
-      // any attachments, stuff it into the text as new lines
-      if (attachments) {
-        attachments.forEach(att=>{
-          debug('adding attachment', att);
-          messages.push(att.text);
-        });
-      }
-      const rawMessage = messages.join('\n').trim();
-      let payload = { roomId: channel };
-
-      try {
-        payload.text = slackdown(rawMessage, this.client.getUsers(), this.client.getChannels());
-        payload.html = converter.makeHtml(payload.text);
-      } catch (e) {
-        debug("could not normalize message", e);
-        payload.text = rawMessage;
-      }
-
-      // lastly, determine the sender
-      if (bot_id) {
-        const bot = this.client.getBotById(bot_id);
-        payload.senderName = bot.name;
-        payload.senderId = bot_id;
-        payload.avatarUrl = bot.icons.image_72
-      } else if (user === "USLACKBOT") {
-        payload.senderName = data.user_profile.name;
-        payload.senderId = data.user;
-        payload.avatarUrl = data.user_profile.image_72;
+      if (data.subtype === "message_changed") {
+        this.createAndSendPayload({
+          channel: data.channel,
+          text: `Edit: ${data.message.text}`,
+          user: data.message.user
+        })
       } else {
-        const isMe = user === this.client.getSelfUserId();
-        payload.senderName = this.client.getUserById(user).name;
-        payload.senderId = isMe ? undefined : user;
+        this.createAndSendPayload({
+          channel: data.channel,
+          text: data.text,
+          attachments: data.attachments,
+          bot_id: data.bot_id,
+          user: data.user,
+          user_profile: data.user_profile,
+        })
       }
+    });
+    debug('registered message listener');
+  }
+  createAndSendPayload(data) {
+    const {
+      channel,
+      text,
+      attachments,
+      bot_id,
+      user,
+      user_profile,
+    } = data;
+    // any direct text
+    let messages = [text];
+    // any attachments, stuff it into the text as new lines
+    if (attachments) attachments.forEach(att=> messages.push(att.text))
 
-      return this.handleThirdPartyRoomMessage(payload).catch(err=>{
+    const rawMessage = messages.join('\n').trim();
+    let payload = { roomId: channel };
+
+    try {
+      payload.text = slackdown(rawMessage, this.client.getUsers(), this.client.getChannels());
+      payload.html = converter.makeHtml(payload.text);
+    } catch (e) {
+      debug("could not normalize message", e);
+      payload.text = rawMessage;
+    }
+
+    // lastly, determine the sender
+    if (bot_id) {
+      const bot = this.client.getBotById(bot_id);
+      payload.senderName = bot.name;
+      payload.senderId = bot_id;
+      payload.avatarUrl = bot.icons.image_72
+    } else if (user === "USLACKBOT") {
+      payload.senderName = user_profile.name;
+      payload.senderId = user;
+      payload.avatarUrl = user_profile.image_72;
+    } else {
+      const isMe = user === this.client.getSelfUserId();
+      payload.senderName = this.client.getUserById(user).name;
+      payload.senderId = isMe ? undefined : user;
+    }
+
+    return this.handleThirdPartyRoomMessage(payload).catch(err=>{
+      console.error(err);
+      this.sendStatusMsg({
+        fixedWidthOutput: true,
+        roomAliasLocalPart: `${this.slackPrefix}_${this.getStatusRoomPostfix()}`
+      }, err.stack).catch((err)=>{
         console.error(err);
       });
     });
-    debug('registered message listener');
   }
   getThirdPartyRoomDataById(id) {
     const directName = (user) => this.client.getUserById(user).name;
