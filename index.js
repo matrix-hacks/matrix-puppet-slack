dc9f0d5ceeb4495f6f3fe5f408ba6ce046729408
Thomas Wouters <thomaswouters@gmail.com>
2019-01-18 23:45:06 +0100

    Register alias namespace

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ new Cli({
       reg.setAppServiceToken(AppServiceRegistration.generateToken());
       reg.setSenderLocalpart(`slack_bot`);
       reg.addRegexPattern("users", `@slack_.*`, true);
+      reg.addRegexPattern("aliases", `@slack_.*`, false);
       callback(reg);
     }).catch(err=>{
       console.error(err.message);
